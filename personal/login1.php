d2fdd952169347468651304f1a468deffb8e8e57
acetennyson <87511451+acetennyson@users.noreply.github.com>
2023-05-02 15:34:57 +0100

    Delete personal directory

--- personal/login1.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-require "connect.php";
-$username = $_POST['username'];
-$password = $_POST['password'];
-login('users',$username,$password);
--- personal/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_session['user']);
-session_destroy();#destroys all sessions
-
-header("Location: index.php");
-#redirecting to login page
-?>
